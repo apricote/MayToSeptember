a36442505cf0afbe6d3bd91c25511a52d7326095
Julian Tölle <julian.toelle97@gmail.com>
2015-03-10 17:03:22 +0100

    Added Controller Load
    Also removed mainInit method

--- src/View/Main.java
+++ src/View/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class Main extends Application {
@@ -26,8 +27,6 @@ public class Main extends Application {
         this.primaryStage.setTitle("MayToSeptember - Stock Optimizer");
 
         initRootLayout();
-
-        initStockView();
     }
 
     private void initRootLayout() {
@@ -41,25 +40,13 @@ public class Main extends Application {
             primaryStage.setScene(scene);
             primaryStage.show();
 
+            // Load the controller
+            WindowController controller = loader.getController();
+            controller.setMainApp(this);
+
         } catch (IOException e) {
             Logger.log("Root Layout could not be loaded", LoggingLevel.ERROR);
             e.printStackTrace();
         }
-    }
-
-    private void initStockView() {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("Main.fxml"));
-
-            AnchorPane stockView = (AnchorPane) loader.load();
-
-            rootLayout.setCenter(stockView);
-
-        } catch (IOException e) {
-            Logger.log("Stock View could not be loaded", LoggingLevel.ERROR);
-            e.printStackTrace();
-        }
-
     }
 }
