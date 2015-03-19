b60b63183cfc6015291154794c3e9d288a6d6b40
Julian Tölle <julian.toelle97@gmail.com>
2015-03-19 02:41:05 +0100

    Removed non necessary cast

--- src/com/toelle/maytoseptember/view/Main.java
+++ src/com/toelle/maytoseptember/view/Main.java
@@ -37,7 +37,7 @@ public class Main extends Application {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("Window.fxml"));
 
-            rootLayout = (AnchorPane) loader.load();
+            rootLayout = loader.load();
 
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
